5ddb6948838ce36206d68cb33e7a04663eff1af1
Jesse <jesseloren@Jesses-iMac.local>
2016-05-20 18:33:58 -0500

    Ccccchanges
    Moving on up

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+	$('.youtube').magnificPopup({
+  delegate: 'a', // child items selector, by clicking on it popup will open
+  type: 'iframe'
+  // other options
+
+
+});
+
+
+});
